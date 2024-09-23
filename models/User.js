8c9a1dc9ddaa2941fdd669129cbdf35db0fc230a
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-23 17:44:52 -0400

    finished the models. started controllers.

--- models/User.js
+++ models/User.js
@@ -1,41 +1,44 @@
 const { Schema, model } = require("mongoose");
-const thoughtSchema = require("./Thought");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must be a valid email address"],
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "thought",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "user",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must be a valid email address"],
     },
-  ],
-});
-
-userSchema.virtual("thoughtCount").get(function () {
-  return this.thoughts.length;
-});
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-const User = model("User", userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
